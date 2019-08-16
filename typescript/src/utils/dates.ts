b6ac59235b78e237d1e3e7a3bf63a5df027b0fda
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2019-08-16 17:09:42 +0100

    confirm fix

--- typescript/src/utils/dates.ts
+++ typescript/src/utils/dates.ts
@@ -11,12 +11,8 @@ export function optionalMsToFormattedString(ms?: string): string | undefined {
 }
 
 export function thirtyMonths(from: Date = new Date()): Date {
-    console.log(`from: ${from.getTime()}`);
     const newDate = new Date(from.getTime());
-    console.log(`newDate: ${newDate.getTime()}`);
     newDate.setUTCMonth(from.getMonth() + 30);
-    console.log(`newDate+30: ${newDate.getTime()}`);
-    console.log(`newDate offset: ${newDate.getUTCDate()}`);
     return newDate;
 }
 
