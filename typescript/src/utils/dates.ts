5537eef42236b71ad122b8e883ef757a86c7e5c3
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2019-08-16 17:02:48 +0100

    add debug to see why TC isn't happy with the test

--- typescript/src/utils/dates.ts
+++ typescript/src/utils/dates.ts
@@ -14,8 +14,9 @@ export function thirtyMonths(from: Date = new Date()): Date {
     console.log(`from: ${from.getTime()}`);
     const newDate = new Date(from.getTime());
     console.log(`newDate: ${newDate.getTime()}`);
-    newDate.setMonth(from.getMonth() + 30);
+    newDate.setUTCMonth(from.getMonth() + 30);
     console.log(`newDate+30: ${newDate.getTime()}`);
+    console.log(`newDate offset: ${newDate.getUTCDate()}`);
     return newDate;
 }
 
